7a98b804f4da904cdecf47ed781d4f5787dbcabb
Ignacio <ignacio.gomez12@mi.unc.edu.ar>
2024-03-05 11:19:12 -0300

    clean

--- hello-rust/target/debug/deps/hello_rust-879952aba296298c.d
+++
@@ -1,5 +0,0 @@
-/home/igna/rust-learning/hello-rust/target/debug/deps/hello_rust-879952aba296298c: src/main.rs
-
-/home/igna/rust-learning/hello-rust/target/debug/deps/hello_rust-879952aba296298c.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/igna/rust-learning/hello-rust/target/debug/hello-rust: /home/igna/rust-learning/hello-rust/src/main.rs
