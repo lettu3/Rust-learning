311828b5bdb3191702c041e625b7fecf7ebd4b03
Ignacio <ignacio.gomez12@mi.unc.edu.ar>
2024-03-05 10:46:12 -0300

    Hello World!

---
+++ hello-rust/target/debug/deps/hello_rust-879952aba296298c.d
@@ -0,0 +1,5 @@
+/home/igna/rust-learning/hello-rust/target/debug/deps/hello_rust-879952aba296298c: src/main.rs
+
+/home/igna/rust-learning/hello-rust/target/debug/deps/hello_rust-879952aba296298c.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/igna/rust-learning/hello-rust/target/debug/hello-rust: /home/igna/rust-learning/hello-rust/src/main.rs
